af89dbb6e7ccc9455f2d81c8d2022956e766c2d8
Dawicl <dawid.pawlak3720@gmail.com>
2023-06-19 20:17:00 +0200

    generowanie_planszy

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-plansza = [[" ", " ", " "],
-           [" ", " ", " "],
-           [" ", " ", " "]]
+plansza = []
+
+def budowanie_planszy():
+    rozmiar = int(input("Podaj rozmiar planszy: "))
+    for _ in range(rozmiar):
+        temp = []
+        for _ in range(rozmiar):
+            temp.append(' ')
+        plansza.append(temp)
 
 def plansza_do_gry():
-    
-    print(plansza[0][0], "|", plansza[0][1], "|", plansza[0][2])
-    print("---------")
-    print(plansza[1][0], "|", plansza[1][1], "|", plansza[1][2])
-    print("---------")
-    print(plansza[2][0], "|", plansza[2][1], "|", plansza[2][2])
+    for j in range(len(plansza)):
+        for i in range(len(plansza)):
+            if i == len(plansza) - 1:
+                print(plansza[j][i])
+            else:
+                print(plansza[j][i], "|", end="")       
+        if j == len(plansza) - 1:
+            continue
+        else:
+            print("---"*len(plansza))
 
 def kolko():
     print("Kolej Gracza 1: O")
@@ -25,23 +35,28 @@ def krzyzyk():
     plansza[a][b] = "X"
 
 def sprawdzenie():
-    for i in range(0, 3):
-        if plansza[i][0] == plansza[i][1] and plansza[i][0] == plansza[i][2] and plansza[i][0] !=' ':
-            print("Koniec gry!")
-            return True
+    for i in range(0, len(plansza)):
+        for j in range(0, len(plansza)):
+            if plansza[j][0] == plansza[j][len(plansza) -1] and plansza[j][0] == plansza[j][1] and plansza[j][0] != " ":
+                print("Koniec gry!")
+                return True
+            elif plansza[0][j] == plansza[len(plansza) -1][j] and plansza[0][j] == plansza[1][j] and plansza[0][j] != " ":
+                print("Koniec gry!")
+                return True
         
-        elif plansza[0][i] == plansza[1][i] and plansza[0][i] == plansza[2][i] and plansza[0][i] !=' ':
-            print("Koniec gry!")
-            return True
+    #         elif plansza[0][i] == plansza[1][i] and plansza[0][i] == plansza[2][i] and plansza[0][i] !=' ':
+    #             print("Koniec gry!")
+    #             return True
 
-    if plansza[0][0] == plansza[1][1] and plansza[0][0] == plansza[2][2] and plansza[0][0] !=' ':
-        print("Koniec gry!")
-        return True
+    # if plansza[0][0] == plansza[1][1] and plansza[0][0] == plansza[2][2] and plansza[0][0] !=' ':
+    #     print("Koniec gry!")
+    #     return True
         
-    elif plansza[2][0] == plansza[1][1] and plansza[2][0] == plansza[0][2] and plansza[2][0] !=' ':
-        print("Koniec gry!")
-        return True
+    # elif plansza[2][0] == plansza[1][1] and plansza[2][0] == plansza[0][2] and plansza[2][0] !=' ':
+    #     print("Koniec gry!")
+    #     return True
 
+budowanie_planszy()
 plansza_do_gry()
 
 while True:
@@ -55,4 +70,3 @@ while True:
     plansza_do_gry()
 
 plansza_do_gry()
-    
